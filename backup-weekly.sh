4b8794d01a664c5f668738b78ae186c4be50ca8a
loidolt <loidolt@gmail.com>
2019-05-10 13:18:50 -0600

    Add cron installation comment

--- backup-weekly.sh
+++ backup-weekly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## Set to run weekly 0 3 * * 0 
+## Set to run weekly 0 3 * * 0 /usr/local/bin/backup-weekly.sh
 
 SITENAME=example
 WEBROOT=/var/www/html/$SITENAME
@@ -9,12 +9,15 @@ DBHOST=$(grep DB_HOST $WEBROOT/public/wp-config.php | awk -F\' '{print$4}')
 DBUSER=$(grep DB_USER $WEBROOT/public/wp-config.php | awk -F\' '{print$4}')
 DBNAME=$(grep DB_NAME $WEBROOT/public/wp-config.php | awk -F\' '{print$4}')
 DBPASSWORD=$(grep DB_PASSWORD $WEBROOT/public/wp-config.php | awk -F\' '{print$4}')
-DBDUMP="$BKPDIR""$DBNAME"_$(date +"%Y-%m-%d-%H-%M").sql
+DBDUMP=/var/tmp/"$DBNAME"_$(date +"%Y-%m-%d-%H-%M").sql
 
 # Backup Database
 mysqldump -h $DBHOST -u $DBUSER -p$DBPASSWORD $DBNAME > $DBDUMP
 
 # Backup Site Files
 
-sudo gsutil -m rsync -zr $WEBROOT gs://$SITENAME-backup/$(date +"%Y-%m-%d")/
-sudo gsutil -m rsync -r $DBDUMP gs://$SITENAME-backup/$(date +"%Y-%m-%d")/
+gsutil -m rsync -r $WEBROOT gs://$SITENAME-backup/$(date +"%Y-%m-%d")/
+gsutil -m rsync $DBDUMP gs://$SITENAME-backup/$(date +"%Y-%m-%d")/
+
+# Remove Temporary Files
+rm $DBDUMP
