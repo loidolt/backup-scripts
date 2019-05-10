382af5832d6a090b12497aa9bf8496943d2ce094
loidolt <loidolt@gmail.com>
2019-05-10 12:28:47 -0600

    Testing new bash script

--- backup-weekly.sh
+++ backup-weekly.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-BKPDIR=/home/loidolt/backup/
 WEBROOT=/var/www/html/
 SITENAME=example
 
@@ -10,16 +9,9 @@ DBNAME=$(grep DB_NAME $WEBROOT/wp-config.php | awk -F\' '{print$4}')
 DBPASSWORD=$(grep DB_PASSWORD $WEBROOT/wp-config.php | awk -F\' '{print$4}')
 DBDUMP="$BKPDIR""$DBNAME"_$(date +"%Y-%m-%d-%H-%M").sql
 
-#In case you want to rsync backups to remote server
-#RUSER=remoteuser
-#RHOST=remoteserver
-#RDIR=/remote/backup/directory/
-#RSSHPORT=22
-
+# Backup Database
 mysqldump -h $DBHOST -u $DBUSER -p$DBPASSWORD $DBNAME > $DBDUMP
 
-tar -czvf "$BKPDIR"wpbackup_$(date +"%Y-%m-%d_%H-%M").tar.gz $WEBROOT $DBDUMP
-
-#rsync -az $BKPDIR -e "ssh -p $RSSHPORT" $RUSER@$RHOST:$RDIR
-
-gsutil -m rsync -r $BKPDIR gs://$SITENAME-backup/monthly/
+# Weekly Data Backup
+sudo gsutil -m rsync -r $WEBROOT/$SITENAME gs://$SITENAME-backup/weekly/$(date +"%Y-%m-%d")/
+sudo gsutil -m rsync -r $DBDUMP gs://$SITENAME-backup/weekly/$(date +"%Y-%m-%d")/
--- old/backup-weekly.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Weekly Data Backup
-sudo gsutil -m rsync -r /var/www/html/example/ gs://example-backup/weekly/
